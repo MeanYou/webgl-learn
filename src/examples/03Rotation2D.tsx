82cacd9ff776505474d6d7683dc9d7d358ff081a
MeanYou <464595889@qq.com>
2022-12-05 21:10:38 +0800

    test rotation

--- src/examples/03Rotation2D.tsx
+++ src/examples/03Rotation2D.tsx
@@ -3,28 +3,29 @@ import React, { useCallback, useEffect } from 'react';
 import { mat4 } from 'gl-matrix';
 import { createProgram } from "@/utils";
 import webglLessonsUI from "webglLessonsUI";
-import OSS from 'ali-oss';
-import axios from "axios";
-
 
 const Example1 = () => {
     useEffect(() => {
         const canvas = document.querySelector('#canvas') as HTMLCanvasElement;
         const gl = canvas.getContext('webgl') as WebGLRenderingContext;
 
-        const vertexSource = `
+        const vertexString = `
             attribute vec2 a_position;
 
             uniform vec2 u_resolution;
             uniform vec2 u_translation;
-            
-            void main() {
-                vec2 position = a_position + u_translation;
+            uniform vec2 u_rotation;
 
-                gl_Position = vec4((position / u_resolution * 2.0 - 1.0) * vec2(1, -1), 0, 1);
+            void main() {
+                vec2 rotatedPosition = vec2(
+                    a_position.x * u_rotation.y + a_position.y * u_rotation.x,
+                    a_position.y * u_rotation.y - a_position.x * u_rotation.x
+                );
+                vec2 position = rotatedPosition + u_translation;
+                gl_Position = vec4((position / u_resolution) * 2.0 - 1.0 * vec2(1, -1), 0, 1);
             }
         `;
-        const fragmentSource = `
+        const fragmentString = `
             precision mediump float;
             uniform vec4 u_color;
 
@@ -32,19 +33,20 @@ const Example1 = () => {
                 gl_FragColor = u_color;
             }
         `;
-        const program = createProgram(gl, vertexSource, fragmentSource);
+
+        const program = createProgram(gl, vertexString, fragmentString);
         if (!program) return;
         gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
         gl.useProgram(program);
 
-        // look up vertex data
+        // look up attribute location
         const positionLocation = gl.getAttribLocation(program, 'a_position');
-        // look up uniforms
+        // look up uniform location
         const resolutionLocation = gl.getUniformLocation(program, 'u_resolution');
+        const rotationLocation = gl.getUniformLocation(program, 'u_rotation');
         const translationLocation = gl.getUniformLocation(program, 'u_translation');
         const colorLocation = gl.getUniformLocation(program, 'u_color');
 
-        // create position buffer
         const positionBuffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
@@ -73,30 +75,41 @@ const Example1 = () => {
         gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
 
         const translation = [0, 0];
-        const width = 100;
-        const height = 30;
-        const color = [Math.random(), Math.random(), Math.random(), 1];
-
-
+        const color = [1, 0, 1, 1];
+        const rotation = [0, 1];
         const drawScene = () => {
             gl.clearColor(0, 0, 0, 255);
             gl.clear(gl.COLOR_BUFFER_BIT);
 
             gl.uniform2f(resolutionLocation, gl.canvas.width, gl.canvas.height);
+            gl.uniform2fv(rotationLocation, rotation);
             gl.uniform2fv(translationLocation, translation);
             gl.uniform4fv(colorLocation, color);
 
             gl.drawArrays(gl.TRIANGLES, 0, 18);
         };
 
-        drawScene();
-
         const updatePosition = (index: number, v: number) => {
             translation[index] = v;
             drawScene();
         };
-        webglLessonsUI.setupSlider('#x', { slide: (e: any, ui: any) => updatePosition(0, ui.value as number), max: gl.canvas.width });
-        webglLessonsUI.setupSlider('#y', { slide: (e: any, ui: any) => updatePosition(1, ui.value as number), max: gl.canvas.height });
+        const updateRotation = (index: number, v: number) => {
+            rotation[index] = v;
+            drawScene();
+        };
+
+        webglLessonsUI.setupSlider('#x', {
+            slide: (e: any, ui: any) => {
+                updatePosition(0, ui.value as number);
+            },
+            max: gl.canvas.width
+        });
+        webglLessonsUI.setupSlider('#y', {
+            slide: (e: any, ui: any) => {
+                updatePosition(1, ui.value as number);
+            },
+            max: gl.canvas.height
+        });
     }, []);
 
     return (<div>
